940ccecc241ff0f22d6750923f04906fa76d811c
Leander Rodrigues <me@leander.xyz>
2019-09-15 23:59:29 -0400

    🚧 Testing vue router stuff

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,15 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Error from "./components/Error.vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes: [{ path: "/404/:id", component: Error }],
+});
 
 new Vue({
   render: (h) => h(App),
